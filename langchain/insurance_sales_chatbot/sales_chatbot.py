d37365b6c2bba333bbb97a63ece966c6963eb1a7
Howard Yang <yyx2114@163.com>
2024-08-13 15:09:52 +0800

    Complete write_qa_pairs.

--- langchain/insurance_sales_chatbot/sales_chatbot.py
+++ langchain/insurance_sales_chatbot/sales_chatbot.py
@@ -4,7 +4,9 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
-
+from langchain_core.tools import tool
+from langchain import hub
+from langchain.agents import AgentExecutor, create_tool_calling_agent
 
 def initialize_sales_bot(vector_store_dir: str="real_scct_agent_test"):
     db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings(api_key="sk-InMcXCrwx83hEtui3d4242A6C7574aC397AdA0EcC07f56E4", base_url="https://api.xiaoai.plus/v1"), allow_dangerous_deserialization=True)
@@ -22,13 +24,55 @@ def initialize_sales_bot(vector_store_dir: str="real_scct_agent_test"):
 def sales_chat(message, history):
     print(f"[message]{message}")
     print(f"[history]{history}")
+
+    # 定义Multiply函数
+    @tool
+    def multiply(first_int: int, second_int: int) -> int:
+        """Multiply two integers together."""
+        print('-------------------------------------------------------------------------------------')
+        return first_int * second_int
+    
+    @tool
+    def write_qa_pair(question: str, answer: str) -> str:
+        """
+            When the user wants to save their response as a QA pair, 
+            the Agent needs to identify the question and answer in the user's response, 
+            then call this function and write it to the file.
+            User Input Sample:
+                [Customer question]Internal Dashboard有几个视图。
+                [System answer]3个。
+            Then call function write_qa_pair(question="Internal Dashboard有几个视图。", answer="3个。")
+        """
+        print('-------------------------------------------------------------------------------------')
+        with open('qa_pairs.txt', 'a', encoding='utf-8') as file:
+            file.write("0.\n")
+            file.write(f"[客户问题] {question}\n")
+            file.write(f"[系统回答] {answer}\n")
+            file.write("\n")
+
+        return "知识问答对补充成功。"
+    
+    tools = [multiply, write_qa_pair]
+    prompt = hub.pull("hwchase17/openai-tools-agent")
+    # 如果没有准确答案，则回复'暂无相关参考'，不要自己编造。如果问的问题不与航运物流业相关，则引导用户问航运物流相关问题。
+    prompt.messages[0].prompt.template = '''
+                                            你是SCCT(Supply chain control tower) Agent，帮助用户回答有关SCCT的相关知识问题。
+                                            如果用户愿意补充你的qa_pairs.txt，那就调用函数写入我们的qa_pairs.txt中。
+                                            )
+                                            '''
+    llm = ChatOpenAI(model_name="gpt-4", temperature=0, base_url="https://api.xiaoai.plus/v1", api_key="sk-InMcXCrwx83hEtui3d4242A6C7574aC397AdA0EcC07f56E4")
+    agent = create_tool_calling_agent(llm, tools, prompt=prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
     # TODO: 从命令行参数中获取
     enable_chat = True
-    llm = ChatOpenAI(model_name="gpt-4", temperature=1, base_url="https://api.xiaoai.plus/v1", api_key="sk-InMcXCrwx83hEtui3d4242A6C7574aC397AdA0EcC07f56E4")
-    messages = [
-        ("system", "你是SCCT Agent，帮助用户回答有关SCCT的相关知识问题。如果没有准确答案，则回复'暂无相关参考'，不要自己编造。如果问的问题不与航运物流业相关，则引导用户问航运物流相关问题。"),
-        ("human", message),
-    ]
+    # messages = [
+    #     ("system", "你是SCCT Agent，帮助用户回答有关SCCT的相关知识问题。如果没有准确答案，则回复'暂无相关参考'，不要自己编造。如果问的问题不与航运物流业相关，则引导用户问航运物流相关问题。"),
+    #     ("human", message),
+    # ]
+    userQuery = {
+        "input":message
+    }
     ans = SALES_BOT({"query": message})
     # 如果检索出结果，或者开了大模型聊天模式
     # 返回 RetrievalQA combine_documents_chain 整合的结果
@@ -36,14 +80,11 @@ def sales_chat(message, history):
         print(f"[result]{ans['result']}")
         print(f"[source_documents]{ans['source_documents']}")
         return ans["result"]
-    elif enable_chat:
-        response = llm.invoke(messages)
-        print(f"[source_documents]{ans['source_documents']}")
-        print(f"[llm_result]{response.content}")
-        return response.content
-    # 否则输出套路话术
     else:
-        return "这个问题我要问问领导"
+        response = agent_executor.invoke(userQuery)
+        print(f"[source_documents]{ans['source_documents']}")
+        print(f"[llm_result]{response['output']}")
+        return response['output']
 
 def launch_gradio():
     demo = gr.ChatInterface(
@@ -57,7 +98,5 @@ def launch_gradio():
     demo.launch(share=True, server_name="0.0.0.0")
 
 if __name__ == "__main__":
-    # 初始化保险销售机器人
     initialize_sales_bot()
-    # 启动 Gradio 服务
     launch_gradio()
